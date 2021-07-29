8ae26bbc4d5112a6802d3ae19ddef4e6183c8a92
Raman Sharma <raman.sharma@msn.com>
2021-07-29 09:21:38 +0100

    ver 0.2.0 release

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'workstation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Raman Sharma'
+maintainer_email 'raman@msn.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 15.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
